5792f82eadc904eef004de1527898ada22537182
yhavin <y.havin@gmail.com>
2026-09-09 17:01:40 -0400

    Add basic route

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return redirect("/contacts")
--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from contact-app!")
-
-
-if __name__ == "__main__":
-    main()
