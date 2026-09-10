dd2307773590318d1234e84d523196e9e01ebb7c
yhavin <y.havin@gmail.com>
2026-09-10 15:22:15 -0400

    Upgrade delete to htmx

--- app.py
+++ app.py
@@ -77,9 +77,9 @@ def contacts_edit_post(contact_id=0):
         return render_template("edit.html", contact=c)
     
 
-@app.route("/contacts/<contact_id>/delete", methods=["POST"])
+@app.route("/contacts/<contact_id>", methods=["DELETE"])
 def contacts_delete(contact_id=0):
     contact = Contact.find(contact_id)
     contact.delete()
     flash("Deleted contact")
-    return redirect("/contacts")
+    return redirect("/contacts", 303)
